cee1924f62667a7fc227ce87f389b1205f8d7aa6
jafet <jafetpintle@gmail.com>
2022-09-10 15:20:34 -0500

    Agregando limites a iteradores en sucesion de Fibonacci

--- iterFibo.py
+++ iterFibo.py
@@ -3,6 +3,9 @@ import time
 
 class IterFibo:
     """Class who calculate the fibonacci sucesion"""
+    def __init__(self , max):
+        self.max = max
+
     def __iter__(self):
         self.first_fib = 1
         self.sec_fib = 1
@@ -10,20 +13,23 @@ class IterFibo:
         return self
     
     def __next__(self):
-        if self.count <=2:
-            self.count +=1
-            return 1
+        if  self.count <= self.max:  
+            if self.count <=2:
+                self.count +=1
+                return 1
+            else:
+                self.result = self.first_fib + self.sec_fib
+                self.first_fib, self.sec_fib = self.sec_fib, self.result
+                self.count+=1
+                return self.result
         else:
-            self.result = self.first_fib + self.sec_fib
-            self.first_fib, self.sec_fib = self.sec_fib, self.result
-            self.count
-            return self.result
+            raise StopIteration
 
 def run():
-    fibonacci = IterFibo()
+    fibonacci = IterFibo(5)
     for elemenet in fibonacci:
         print(elemenet)
-        time.sleep(1)
+        time.sleep(0.5)
 
 if __name__ == "__main__":
     run()
